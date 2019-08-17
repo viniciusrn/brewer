8ffbebc346b803f62b1178a74b132f8bec57aa8c
Vinicius Riemma <viniciusriemma@gmail.com>
2019-08-17 17:20:21 -0300

    Revisão

--- target/classes/static/javascripts/brewer.js
+++
@@ -1,7 +0,0 @@
-$(function(){
-	var decimal = $('.js-decimal');
-	decimal.maskMoney({decimal: ',', thousands: '.'});
-	
-	var plain = $('.js-plain');
-	plain.maskMoney({ precision: 0,  thousands: '.'});
-});
