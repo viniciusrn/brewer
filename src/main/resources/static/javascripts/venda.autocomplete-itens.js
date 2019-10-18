c6481c241a16b103bbe3edfd0a5d437cdb5505c9
Vinicius Riemma <viniciusriemma@gmail.com>
2019-10-17 22:49:55 -0300

    Atualizando vendas de cerveja

--- src/main/resources/static/javascripts/venda.autocomplete-itens.js
+++ src/main/resources/static/javascripts/venda.autocomplete-itens.js
@@ -6,13 +6,15 @@ Brewer.Autocomplete = (function() {
 		this.skuOuNomeInput = $('.js-sku-nome-cerveja-input');
 		var htmlTemplateAutocomplete = $('#template-autocomplete-cerveja').html();
 		this.template = Handlebars.compile(htmlTemplateAutocomplete);
+		this.emitter = $({});
+		this.on = this.emitter.on.bind(this.emitter);
 	}
 	
 	Autocomplete.prototype.iniciar = function() {
 		var options = {
 			url: function(skuOuNome) {
-				return '/brewer/cerveja?skuOuNome=' + skuOuNome;
-			},
+				return this.skuOuNomeInput.data('url') + '?skuOuNome=' + skuOuNome;
+			}.bind(this),
 			getValue: 'nome',
 			minCharNumber: 3,
 			requestDelay: 300,
@@ -21,23 +23,27 @@ Brewer.Autocomplete = (function() {
 			},
 			template: {
 				type: 'custom',
-				method: function(nome, cerveja) {
-					cerveja.valorFormatado = Brewer.formatarMoeda(cerveja.valor);
-					return this.template(cerveja);
-				}.bind(this)
+				method: template.bind(this)
+			},
+			list: {
+				onChooseEvent: onItemSelecionado.bind(this)
 			}
 		};
 		
 		this.skuOuNomeInput.easyAutocomplete(options);
 	}
 	
+	function onItemSelecionado() {
+		this.emitter.trigger('item-selecionado', this.skuOuNomeInput.getSelectedItemData());
+		this.skuOuNomeInput.val('');
+		this.skuOuNomeInput.focus();
+	}
+	
+	function template(nome, cerveja) {
+		cerveja.valorFormatado = Brewer.formatarMoeda(cerveja.valor);
+		return this.template(cerveja);
+	}
+	
 	return Autocomplete
 	
 }());
-
-$(function() {
-	
-	var autocomplete = new Brewer.Autocomplete();
-	autocomplete.iniciar();
-	
-})
